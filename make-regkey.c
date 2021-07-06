97783e52431327e5da52974187fde6ec43883fa1
Anthony Refuerzo <anthony96922@gmail.com>
2021-07-06 12:35:12 -0700

    Move everything to main

--- make-regkey.c
+++ make-regkey.c
@@ -17,11 +17,6 @@
  *
  */
 
-/*
- * This generates valid keys for a given name. Generated keys will be
- * recognized, but the unregistered message still plays every 12 hours.
- */
-
 #include <stdio.h>
 #include <string.h>
 #include <getopt.h>
@@ -37,115 +32,23 @@
 #define FEATURE_BYTE	0xff
 #define FEATURE_INT	0xffffbfff
 
-int make_key(char *name, char *out_key, unsigned int features) {
-	// hex representation of one byte (2 chars + terminator)
-	char key_byte[3];
-
-	// key checksum
-	int checksum;
-
-	// make sure we don't try to divide by 0
-	int div_check;
-
-	// the length of the name
-	int len = strlen(name);
-
-	// 15 = the stuff before and after the key (112233445566778899<name>aabbccddeeff)
-	int key_len = len + 15;
-
-	char *key;
-	key = malloc(key_len*2+3);
-	memset(key, 0, key_len*2+3);
-
-	// the locations of the important parts
-	char *key_features	= key + 1; // licensed features
-	char *key_checksum	= key + 5; // not sure how this is calculated
-	char *key_name		= key + 9; // display name
-	char *key_trailer	= key_name + len; // extra stuff
-
-	// the key prefix: one 1-byte int and two 4-byte ints (9 chars)
-	key[0] = FEATURE_BYTE;
-
-	memcpy(key_features, &features, sizeof(int));
-
-	// copy name to key body
-	memcpy(key_name, name, len);
-
-	div_check = (key_name[2] - key_name[3]) + 1;
-
-	if (div_check == 0) {
-		// we can't divide by 0
-		fprintf(stderr, "Invalid name.\n");
-		return 1;
-	}
-
-	div_check = (key_name[0] * key_name[1]) / div_check;
-
-	key_trailer[1] = (((key_name[0] | key_name[1]) ^ ((key_name[2] | key_name[3]) + key_name[4])) & 0xf) << 4;
-	key_trailer[1] |= (key_name[0] ^ key_name[1] ^ key_name[2] ^ key_name[3] ^ key_name[4]) & 0xf;
-	key_trailer[2] = ((div_check - key_name[4]) & 0xf) << 4;
-	key_trailer[2] |= (div_check * key_name[4]) & 0xf;
-	key_trailer[3] = ((key_name[2] - key_name[3]) * (key_name[0] + key_name[1]) ^ key_name[4]) & 0xf;
-	key_trailer[3] |= (((key_name[2] + key_name[3]) * (key_name[0] - key_name[1]) ^ ~key_name[4]) & 0xf) << 4;
-
-	int uVar8 = 192; // how is this calculated?
-	int bVar3 = uVar8;
-	if ((((key_name[0] + key_name[1] + key_name[2] - key_name[3] + key_name[4]) & 0xf) ^ uVar8) & 8) {
-		bVar3 ^= 8;
-	}
-
-	key_trailer[4] = bVar3;
-
-	key_trailer[5] = ((key_name[0] + key_name[1] - key_name[2]) - (key_name[3] + key_name[4])) & 0xf;
-	key_trailer[5] |= 15 << 4;
-
-	checksum = 0;
-
-	// calculate the checksum
-	for (int i = 0; i < key_len; i++) {
-		checksum = key[i] * 0x11121 + (checksum << 3);
-		checksum += checksum >> 26;
-	}
-
-	memcpy(key_checksum, &checksum, sizeof(int));
-
-	// encode the key
-	char tmp1, tmp2;
-	for (int i = 0; i < key_len; i++) {
-		tmp1 = key[i] ^ ((-1 - i) - (1 << (1 << (i & 31) & 7)));
-		tmp2 = 0;
-		for (int j = 0; j < 8; j++) {
-			tmp2 <<= 1;
-			tmp2 |= tmp1 & 1;
-			tmp1 >>= 1;
-		}
-		key[i] = tmp2;
-	}
-
-	out_key[0] = '<';
-
-	for (int i = 0; i < key_len; i++) {
-		sprintf(key_byte, "%.2x", key[i]);
-		strcat(out_key, key_byte);
-	}
-
-	out_key[key_len*2+1] = '>';
-
-	free(key);
-
-	return 0;
-}
-
 int main(int argc, char *argv[]) {
 	int opt;
-	int ret;
 	unsigned int features = FEATURE_INT;
 	int name_len;
 	int default_name = 1;
 	int default_features = 1;
-
 	char name[MAXLEN];
-	char reg_key[MAXLEN*4] = {0};
+	char *key;
+	char *out_key;
+
+	// hex representation of one byte (2 chars + terminator)
+	char out_key_byte[3];
+
+	int key_len;
+
+	// key checksum
+	int checksum;
 
 	const char *short_opt = "n:f:h";
 	const struct option long_opt[] = {
@@ -202,11 +105,89 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	ret = make_key(name, reg_key, features);
+	// 15 = the stuff before and after the key (112233445566778899<name>aabbccddeeff)
+	key_len = name_len + 15;
 
-	if (ret == 0) {
-		printf("%s\n", reg_key);
+	key = malloc(key_len*2);
+	out_key = malloc(key_len*2+3);
+	memset(key, 0, key_len*2);
+	memset(out_key, 0, key_len*2+3);
+
+	// the locations of the important parts
+	char *key_features	= key + 1; // licensed features
+	char *key_checksum	= key + 5; // not sure how this is calculated
+	char *key_name		= key + 9; // display name
+	char *key_trailer	= key_name + name_len; // extra stuff
+
+	// registered options
+	key[0] = FEATURE_BYTE;
+	memcpy(key_features, &features, sizeof(int));
+
+	// copy name to key
+	memcpy(key_name, name, name_len);
+
+	// make sure we don't try to divide by 0
+	if ((key_name[2] - key_name[3]) + 1 == 0) {
+		// we can't divide by 0
+		fprintf(stderr, "Invalid name.\n");
+		return 1;
 	}
 
-	return ret;
+	key_trailer[1] = (((key_name[0] | key_name[1]) ^ ((key_name[2] | key_name[3]) + key_name[4])) & 0xf) << 4;
+	key_trailer[1] |= (key_name[0] ^ key_name[1] ^ key_name[2] ^ key_name[3] ^ key_name[4]) & 0xf;
+	key_trailer[2] = (((key_name[0] * key_name[1]) / ((key_name[2] - key_name[3]) + 1) - key_name[4]) & 0xf) << 4;
+	key_trailer[2] |= ((key_name[0] * key_name[1]) / ((key_name[2] - key_name[3]) + 1) * key_name[4]) & 0xf;
+	key_trailer[3] = ((key_name[2] - key_name[3]) * (key_name[0] + key_name[1]) ^ key_name[4]) & 0xf;
+	key_trailer[3] |= (((key_name[2] + key_name[3]) * (key_name[0] - key_name[1]) ^ ~key_name[4]) & 0xf) << 4;
+
+	int uVar8 = 192; // how is this calculated?
+	int bVar3 = uVar8;
+	if ((((key_name[0] + key_name[1] + key_name[2] - key_name[3] + key_name[4]) & 0xf) ^ uVar8) & 8) {
+		bVar3 ^= 8;
+	}
+
+	key_trailer[4] = bVar3;
+
+	key_trailer[5] = ((key_name[0] + key_name[1] - key_name[2]) - (key_name[3] + key_name[4])) & 0xf;
+	key_trailer[5] |= 15 << 4;
+
+	checksum = 0;
+
+	// calculate the checksum
+	for (int i = 0; i < key_len; i++) {
+		checksum = key[i] * 0x11121 + (checksum << 3);
+		checksum += checksum >> 26;
+	}
+
+	// copy the checksum
+	memcpy(key_checksum, &checksum, sizeof(int));
+
+	// encode the key
+	char tmp1, tmp2;
+	for (int i = 0; i < key_len; i++) {
+		tmp1 = key[i] ^ ((-1 - i) - (1 << (1 << (i & 31) & 7)));
+		tmp2 = 0;
+		for (int j = 0; j < 8; j++) {
+			tmp2 <<= 1;
+			tmp2 |= tmp1 & 1;
+			tmp1 >>= 1;
+		}
+		key[i] = tmp2;
+	}
+
+	out_key[0] = '<';
+
+	for (int i = 0; i < key_len; i++) {
+		sprintf(out_key_byte, "%02x", key[i]);
+		strcat(out_key, out_key_byte);
+	}
+
+	out_key[key_len*2+1] = '>';
+
+	printf("%s\n", out_key);
+
+	free(key);
+	free(out_key);
+
+	return 0;
 }
