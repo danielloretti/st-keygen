fd8c85a802459bbe6efc5302340166ac80940d6b
Anthony Refuerzo <anthony96922@gmail.com>
2022-04-17 15:53:52 -0700

    Update

--- make-regkey.c
+++ make-regkey.c
@@ -25,7 +25,7 @@
 //#define DUMP_BITS
 
 // max name length
-#define MAXLEN		64
+#define MAXLEN		108
 
 // uncomment for event FM license (3 days)
 //#define EVENT_FM
@@ -83,79 +83,86 @@ int main(int argc, char *argv[]) {
 	int opt;
 	unsigned int features = FEATURES;
 	int name_len;
-	int name_set = 0;
-	int default_features = 1;
 	int key_len;
 	// key checksum
 	int checksum;
 	char name[MAXLEN+1];
-	unsigned char key[(MAXLEN+1)*4];
-	char out_key[(MAXLEN+1)*4];
+	unsigned char key[9+MAXLEN+1+8];
+	char out_key_text[(9+MAXLEN+1+8)*2];
 
-	const char *short_opt = "n:f:h";
+	unsigned char *key_features;
+	unsigned char *key_checksum;
+	unsigned char *key_name;
+	unsigned char *key_trailer;
+
+	const char *short_opt = "f:h";
 	const struct option long_opt[] = {
-		{"name",	required_argument,	NULL,	'n'},
 		{"features",	required_argument,	NULL,	'f'},
 		{"help",	no_argument,		NULL,	'h'},
 		{0,		0,			0,	0}
 	};
 
 	memset(name, 0, MAXLEN+1);
-	memset(key, 0, (MAXLEN+1)*4);
-	memset(out_key, 0, (MAXLEN+1)*4);
+	memset(key, 0, 9+MAXLEN+1+8);
+	memset(out_key_text, 0, (9+MAXLEN+1+8)*2);
 
-	while ((opt = getopt_long(argc, argv, short_opt, long_opt, NULL)) != -1) {
-		switch (opt) {
-			case 'n':
-				strncpy(name, optarg, MAXLEN);
-				printf("Using name \"%s\".\n", name);
-				name_set = 1;
-				break;
-			case 'f':
-				features = strtoul(optarg, NULL, 16);
-				printf("Using custom feature config (0x%08x)\n", features);
-				default_features = 0;
-				break;
-			case 'h':
-			case '?':
-			default:
-				fprintf(stderr,
-					"Stereo Tool key generator\n"
-					"\n"
-					"Usage: %s -n name [ -f features (hex) ]\n"
-					"\n"
-					"\t-n name\t\tName to use for key (required)\n"
-					"\t-f features\tRegistered options in hexadecimal\n"
-					"\n",
-				argv[0]);
-				return 1;
-		}
+keep_parsing_opts:
+
+	opt = getopt_long(argc, argv, short_opt, long_opt, NULL);
+	if (opt == -1) goto done_parsing_opts;
+
+	switch (opt) {
+		case 'f':
+			features = strtoul(optarg, NULL, 16);
+			break;
+
+		case 'h':
+		case '?':
+		default:
+			fprintf(stderr,
+				"Stereo Tool key generator\n"
+				"\n"
+				"Generates a valid registration key for a given name\n"
+				"\n"
+				"Usage: %s [ -f features (hex) ] NAME\n"
+				"\n"
+				"\t-f features\tRegistered options in hexadecimal (optional)\n"
+				"\n",
+			argv[0]);
+			return 1;
 	}
 
-	if (!name_set) {
+	goto keep_parsing_opts;
+
+done_parsing_opts:
+
+	if (optind < argc) {
+		name_len = strlen(argv[optind]);
+		if (name_len > MAXLEN) {
+			printf("Name is too long.\n");
+			return 1;
+		}
+		strncat(name, argv[optind], MAXLEN);
+	}
+
+	if (!name[0]) {
 		printf("Please specify a name.\n");
 		return 1;
 	}
-
-	name_len = strlen(name);
 
 	// input validation
 
 	// needed for name validation
 	if (name_len < 5) {
-		fprintf(stderr, "Name must be at least 5 characters long.\n");
+		printf("Name must be at least 5 characters long.\n");
 		return 1;
 	}
 
 	// make sure we don't try to divide by 0
 	if ((name[2] - name[3]) + 1 == 0) {
 		// we can't divide by 0
-		fprintf(stderr, "Invalid name.\n");
+		printf("Invalid name.\n");
 		return 1;
-	}
-
-	if (default_features) {
-		printf("Using default features (0x%08x)\n", features);
 	}
 
 #ifdef DUMP_BITS
@@ -169,10 +176,10 @@ int main(int argc, char *argv[]) {
 	key_len = 9 + name_len + 1 /* null terminator for name string */ + 8;
 
 	// the locations of the important parts
-	unsigned char *key_features	= key + 1; // licensed features
-	unsigned char *key_checksum	= key + 5;
-	unsigned char *key_name		= key + 9; // display name
-	unsigned char *key_trailer	= key_name + name_len + 1; // name validation
+	key_features	= key + 1; // licensed features
+	key_checksum	= key + 5;
+	key_name	= key + 9; // display name
+	key_trailer	= key_name + name_len + 1; // name validation
 
 	key[0] = 1; // doesn't seem to affect anything
 
@@ -218,15 +225,18 @@ int main(int argc, char *argv[]) {
 			out |= in & 1;
 			in >>= 1;
 		}
-		key[i] = out;
+		sprintf(out_key_text+i*2, "%02x", out);
 	}
 
 	// output
-	for (int i = 0; i < key_len; i++) {
-		sprintf(out_key+i*2, "%02x", key[i]);
-	}
-
-	printf("<%s>\n", out_key);
+	printf("\n");
+	printf("==========================================\n");
+	printf("Name\t\t: %s\n", name);
+	printf("Features\t: 0x%08x\n", features);
+	printf("==========================================\n");
+	printf("\n");
+	printf("<%s>\n", out_key_text);
+	printf("\n");
 
 	return 0;
 }
